59dde345ffddc7131f842bcddbee23a9f1ec6fd3
Ante Baric <ante@kickass.website>
2021-11-30 23:46:46 +0100

    refactor: format code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::io;
 
 struct Utils;
 
 impl Utils {
-     // i know there is a list.sort, but for practice sake
+    // i know there is a list.sort, but for practice sake
     fn sort(list: &mut Vec<i64>) -> Vec<i64> {
         for index in 0..list.len() {
             for index2 in 0..list.len() - 1 - index {
@@ -16,14 +16,14 @@ impl Utils {
                     list[index2 + 1] = temp;
                 }
             }
-        }   
+        }
 
         list.to_vec()
     }
 }
 
 struct Program {
-    math: ProgramMath
+    math: ProgramMath,
 }
 
 struct ProgramMath;
@@ -60,42 +60,33 @@ impl ProgramMath {
 
 impl Program {
     fn init() -> Program {
-        Program {
-            math: ProgramMath
-        }
+        Program { math: ProgramMath }
     }
 
     fn fahrenheit_to_celsius(&self, value: f64) -> f64 {
         const RATIO: f64 = 5.0 / 9.0;
         let celsius = value - 32.0;
         let celsius = celsius * RATIO;
-    
-        return celsius
+
+        return celsius;
     }
-    
-    fn guessing_game(&self, ) {
+
+    fn guessing_game(&self) {
         println!("Guess the number!");
-    
         let secret_number = rand::thread_rng().gen_range(1..101);
-    
         // println!("The secret number is: {}", secret_number);
-    
         loop {
             println!("Please input your guess.");
-    
             let mut guess = String::new();
-        
             io::stdin()
                 .read_line(&mut guess)
                 .expect("Failed to read line");
-        
-                let guess: u32 = match guess.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => continue,
-                };
-        
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
             println!("You guessed: {}", guess);
-        
             match guess.cmp(&secret_number) {
                 Ordering::Less => println!("Too small!"),
                 Ordering::Greater => println!("Too big!"),
@@ -106,7 +97,6 @@ impl Program {
             }
         }
     }
-    
     fn fibonacci(&self, num: u128, memo: &mut HashMap<u128, u128>) -> u128 {
         if memo.contains_key(&num) {
             memo[&num]
@@ -115,14 +105,15 @@ impl Program {
         } else {
             let result = self.fibonacci(num - 1, memo) + self.fibonacci(num - 2, memo);
             memo.insert(num, result);
-    
             result
         }
     }
-    
     fn twelve_days_of_xmas(&self) {
         for day in 0..12 {
-            println!("On the {}. day of Christmas, my true love sent to me...", day + 1)
+            println!(
+                "On the {}. day of Christmas, my true love sent to me...",
+                day + 1
+            )
         }
     }
 
@@ -140,10 +131,15 @@ impl Program {
 
         let rectangle = Rectangle {
             width: 69,
-            height: 69
+            height: 69,
         };
 
-        println!("Area of {}x{} rectangle is {}", rectangle.width, rectangle.height, rectangle.area())
+        println!(
+            "Area of {}x{} rectangle is {}",
+            rectangle.width,
+            rectangle.height,
+            rectangle.area()
+        )
     }
 
     fn pig_latin(&self, text: String) -> String {
@@ -164,7 +160,7 @@ impl Program {
                         extension.push(letter);
                         extension.push_str("ay");
 
-                        continue
+                        continue;
                     }
                 }
 
@@ -184,7 +180,6 @@ fn main() {
 
     println!("Select program");
     let mut selection = String::new();
-    
     io::stdin()
         .read_line(&mut selection)
         .expect("Failed to read line");
@@ -193,77 +188,79 @@ fn main() {
         "GuessingGame" => program.guessing_game(),
         "FahrenheitToCelsius" => {
             let mut value = String::new();
-    
             println!("Input celsius value");
 
             io::stdin()
                 .read_line(&mut value)
                 .expect("Failed to read line");
-        
-                match value.trim().parse() {
-                    Ok(num) => {
-                        println!("Celsius {}", program.fahrenheit_to_celsius(num))
-                    },
-                    Err(_) => {
-                        println!("Not a number")
-                    },
-                };
+
+            match value.trim().parse() {
+                Ok(num) => {
+                    println!("Celsius {}", program.fahrenheit_to_celsius(num))
+                }
+                Err(_) => {
+                    println!("Not a number")
+                }
+            };
         }
         "Fibonacci" => {
             let mut value = String::new();
-    
             println!("How many steps?");
 
             io::stdin()
                 .read_line(&mut value)
                 .expect("Failed to read line");
-        
-                match value.trim().parse() {
-                    Ok(num) => {
-                        println!("{}", program.fibonacci(num, &mut HashMap::new()))
-                    },
-                    Err(_) => {
-                        println!("Not a number")
-                    },
-                };
+
+            match value.trim().parse() {
+                Ok(num) => {
+                    println!("{}", program.fibonacci(num, &mut HashMap::new()))
+                }
+                Err(_) => {
+                    println!("Not a number")
+                }
+            };
         }
         "12DaysOfXmas" => program.twelve_days_of_xmas(),
         "Rectangle" => program.rectangle(),
         "Mean" => {
             let mut value = String::new();
-    
             println!("Input Comma-separated list");
 
             io::stdin()
                 .read_line(&mut value)
                 .expect("Failed to read line");
-        
-            let list: Vec<i64> = value.trim().split(",").map(|s| s.parse().expect("Parse failed")).collect();
+
+            let list: Vec<i64> = value
+                .trim()
+                .split(",")
+                .map(|s| s.parse().expect("Parse failed"))
+                .collect();
             println!("{}", program.math.mean(&list));
-        },
+        }
         "Median" => {
             let mut value = String::new();
-    
             println!("Input Comma-separated list");
 
             io::stdin()
                 .read_line(&mut value)
                 .expect("Failed to read line");
-        
-            let list: Vec<i64> = value.trim().split(",").map(|s| s.parse().expect("Parse failed")).collect();
+
+            let list: Vec<i64> = value
+                .trim()
+                .split(",")
+                .map(|s| s.parse().expect("Parse failed"))
+                .collect();
             println!("{}", program.math.median(&list));
-        },
+        }
         "PigLatin" => {
             let mut value = String::new();
-    
             println!("Input some text");
 
             io::stdin()
                 .read_line(&mut value)
                 .expect("Failed to read line");
-        
             println!("{}", program.pig_latin(String::from(value.trim())));
-        },
+        }
         _ => {
             println!("404");
         }
